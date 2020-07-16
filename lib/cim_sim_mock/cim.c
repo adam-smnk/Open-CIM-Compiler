1e99680878bbcb1df507ef4ba0168224444ec724
Adam Siemieniuk <a.i.siemieniuk@student.tue.nl>
2020-07-16 13:59:32 +0200

    GEMM on mock CIM simulator
    Adds naive GEMM implementation to the mocked CIM SIM GEMM operation.
    CIM crossbar precision (bit size) is fixed within the mock simulator
    itself.

--- lib/cim_sim_mock/cim.c
+++ lib/cim_sim_mock/cim.c
@@ -2,16 +2,36 @@
 
 #include <stdio.h>
 
+// Use fixed crossbar precision
+uint32_t *xbar;
+
 void cim_await(uint32_t cim_id) { printf("CIM Computation completed\n"); }
 
 void cim_configure_crossbar(uint32_t cim_id, uint8_t **B, uint8_t *bias,
                             uint32_t N, uint32_t K) {
   printf("CIM Crossbar configured\n");
+
+  xbar = (uint32_t *)B;
 }
 
 void cim_gemm(uint32_t cim_id, uint8_t *A, int M, int N, int K, int K_lda,
               int x_pos, int y_pos, uint8_t *C, int N_ldc) {
   printf("CIM GEMM started...\n");
+
+  uint32_t *matA = (uint32_t *)A;
+  uint32_t *matC = (uint32_t *)C;
+
+  for (int m = 0; m < M; ++m) {
+    for (int n = 0; n < N; ++n) {
+      uint32_t sum = 0;
+
+      for (int k = 0; k < K; ++k) {
+        sum += matA[m * K + k] * xbar[k * N + n];
+      }
+
+      matC[m * N + n] = sum;
+    }
+  }
 }
 
 void cim_gevm(uint32_t cim_id, uint8_t *A, int K, int N, int x_pos, int y_pos,
