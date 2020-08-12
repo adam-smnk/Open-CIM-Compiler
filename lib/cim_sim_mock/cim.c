ae9cb8b050eda1ed21d6b9b5ae6181b44bb627c9
Adam Siemieniuk <a.i.siemieniuk@student.tue.nl>
2020-08-12 08:03:58 +0200

    Improve mock CIM SIM debug messages
    Improves readability of CIM debug messages.

--- lib/cim_sim_mock/cim.c
+++ lib/cim_sim_mock/cim.c
@@ -5,18 +5,20 @@
 // Use fixed crossbar precision
 static uint32_t *xbar;
 
-void cim_await(uint32_t cim_id) { printf("CIM Computation completed\n"); }
+void cim_await(uint32_t cim_id) {
+  printf("[CIM Info] Computation completed\n");
+}
 
 void cim_configure_crossbar(uint32_t cim_id, uint8_t **B, uint8_t *bias,
                             uint32_t N, uint32_t K) {
-  printf("CIM Crossbar configured\n");
+  printf("[CIM Info] Crossbar configured\n");
 
   xbar = (uint32_t *)B;
 }
 
 void cim_gemm(uint32_t cim_id, uint8_t *A, int M, int N, int K, int K_lda,
               int x_pos, int y_pos, uint8_t *C, int N_ldc) {
-  printf("CIM GEMM started...\n");
+  printf("[CIM Info] GEMM started...\n");
 
   uint32_t *matA = (uint32_t *)A;
   uint32_t *matC = (uint32_t *)C;
@@ -36,7 +38,7 @@ void cim_gemm(uint32_t cim_id, uint8_t *A, int M, int N, int K, int K_lda,
 
 void cim_gevm(uint32_t cim_id, uint8_t *A, int K, int N, int x_pos, int y_pos,
               uint8_t *C) {
-  printf("CIM GEVM started...\n");
+  printf("[CIM Info] GEVM started...\n");
 
   uint32_t *matA = (uint32_t *)A;
   uint32_t *matC = (uint32_t *)C;
