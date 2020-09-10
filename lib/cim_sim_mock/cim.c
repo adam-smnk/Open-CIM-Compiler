015f4d815ae65d44235a38fa5a449b96c6741e58
Adam Siemieniuk <a.i.siemieniuk@student.tue.nl>
2020-09-10 14:57:45 +0200

    CIM mock sim refactor
    Adds a define to control CIM crossbar precision to make changing it
    easier.

--- lib/cim_sim_mock/cim.c
+++ lib/cim_sim_mock/cim.c
@@ -2,8 +2,10 @@
 
 #include <stdio.h>
 
+#define XBAR_PRECISION uint32_t
+
 // Use fixed crossbar precision
-static uint32_t *xbar;
+static XBAR_PRECISION *xbar;
 
 void cim_await(uint32_t cim_id) {
   printf("[CIM Info] Computation completed\n");
@@ -13,19 +15,19 @@ void cim_configure_crossbar(uint32_t cim_id, uint8_t **B, uint8_t *bias,
                             uint32_t N, uint32_t K) {
   printf("[CIM Info] Crossbar configured\n");
 
-  xbar = (uint32_t *)B;
+  xbar = (XBAR_PRECISION *)B;
 }
 
 void cim_gemm(uint32_t cim_id, uint8_t *A, int M, int N, int K, int K_lda,
               int x_pos, int y_pos, uint8_t *C, int N_ldc) {
   printf("[CIM Info] GEMM started...\n");
 
-  uint32_t *matA = (uint32_t *)A;
-  uint32_t *matC = (uint32_t *)C;
+  XBAR_PRECISION *matA = (XBAR_PRECISION *)A;
+  XBAR_PRECISION *matC = (XBAR_PRECISION *)C;
 
   for (int m = 0; m < M; ++m) {
     for (int n = 0; n < N; ++n) {
-      uint32_t sum = 0;
+      XBAR_PRECISION sum = 0;
 
       for (int k = 0; k < K; ++k) {
         sum += matA[m * K + k] * xbar[k * N + n];
@@ -40,11 +42,11 @@ void cim_gevm(uint32_t cim_id, uint8_t *A, int K, int N, int x_pos, int y_pos,
               uint8_t *C) {
   printf("[CIM Info] GEVM started...\n");
 
-  uint32_t *matA = (uint32_t *)A;
-  uint32_t *matC = (uint32_t *)C;
+  XBAR_PRECISION *matA = (XBAR_PRECISION *)A;
+  XBAR_PRECISION *matC = (XBAR_PRECISION *)C;
 
   for (int n = 0; n < N; ++n) {
-    uint32_t sum = 0;
+    XBAR_PRECISION sum = 0;
 
     for (int k = 0; k < K; ++k) {
       sum += matA[k] * xbar[k * N + n];
