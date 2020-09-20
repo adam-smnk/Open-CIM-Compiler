67f2f82ed7d4de27bdba4ff8b2740d8671803bc0
Adam Siemieniuk <a.i.siemieniuk@student.tue.nl>
2020-09-20 22:48:57 +0200

    CIM Mock SIM multiple tiles
    Adds support for multiple tiles to the mock CIM simulator.
    Passed tile ID is now used to select appropriate tile.

--- lib/cim_sim_mock/cim.c
+++ lib/cim_sim_mock/cim.c
@@ -6,26 +6,27 @@
 #define TEXT_RESET_COLOR "\033[0m"
 
 #define XBAR_PRECISION uint32_t
+#define NUM_CIM_TILES 16
 
 // Use fixed crossbar precision
-static XBAR_PRECISION *xbar;
+static XBAR_PRECISION *xbar[NUM_CIM_TILES];
 
 void cim_await(uint32_t cim_id) {
-  printf("[CIM Info] Computation completed\n");
+  printf("[CIM %d Info] Computation completed\n", cim_id);
 }
 
 void cim_configure_crossbar(uint32_t cim_id, uint8_t **B, uint8_t *bias,
                             uint32_t N, uint32_t K) {
   printf(TEXT_SET_COLOR);
-  printf("[CIM Info] Crossbar configured\n");
+  printf("[CIM %d Info] Crossbar configured\n", cim_id);
   printf(TEXT_RESET_COLOR);
 
-  xbar = (XBAR_PRECISION *)B;
+  xbar[cim_id] = (XBAR_PRECISION *)B;
 }
 
 void cim_gemm(uint32_t cim_id, uint8_t *A, int M, int N, int K, int K_lda,
               int x_pos, int y_pos, uint8_t *C, int N_ldc) {
-  printf("[CIM Info] GEMM started...\n");
+  printf("[CIM %d Info] GEMM started...\n", cim_id);
 
   XBAR_PRECISION *matA = (XBAR_PRECISION *)A;
   XBAR_PRECISION *matC = (XBAR_PRECISION *)C;
@@ -35,7 +36,7 @@ void cim_gemm(uint32_t cim_id, uint8_t *A, int M, int N, int K, int K_lda,
       XBAR_PRECISION sum = 0;
 
       for (int k = 0; k < K; ++k) {
-        sum += matA[m * K + k] * xbar[k * N + n];
+        sum += matA[m * K + k] * ((xbar[cim_id])[k * N + n]);
       }
 
       matC[m * N + n] = sum;
@@ -45,7 +46,7 @@ void cim_gemm(uint32_t cim_id, uint8_t *A, int M, int N, int K, int K_lda,
 
 void cim_gevm(uint32_t cim_id, uint8_t *A, int K, int N, int x_pos, int y_pos,
               uint8_t *C) {
-  printf("[CIM Info] GEVM started...\n");
+  printf("[CIM %d Info] GEVM started...\n", cim_id);
 
   XBAR_PRECISION *matA = (XBAR_PRECISION *)A;
   XBAR_PRECISION *matC = (XBAR_PRECISION *)C;
@@ -54,7 +55,7 @@ void cim_gevm(uint32_t cim_id, uint8_t *A, int K, int N, int x_pos, int y_pos,
     XBAR_PRECISION sum = 0;
 
     for (int k = 0; k < K; ++k) {
-      sum += matA[k] * xbar[k * N + n];
+      sum += matA[k] * ((xbar[cim_id])[k * N + n]);
     }
 
     matC[n] = sum;
